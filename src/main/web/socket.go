156a0f565bfb291fa12b0c0e0fc98ceeea96027d
caoheng <caoheng@youlu.com>
2023-03-09 15:03:57 +0800

    feat: UDPSocket示例代码

--- src/main/web/socket.go
+++ src/main/web/socket.go
@@ -6,6 +6,8 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -149,3 +151,148 @@ func serverHandler() {
 		conn.Close()
 	}
 }
+
+/*
+serverHandler2
+上述代码有个缺点，执行的时候是单任务的，不能同时接收多个请求，下面利用goroutine机制进行改造
+通过把业务处理分离到函数handleClient, 再加上go关键字就实现了服务端的多并发
+*/
+func serverHandler2() {
+	service := ":1200"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	daytime := time.Now().String()
+	conn.Write([]byte(daytime))
+}
+
+/*
+上述服务端代码并没有处理实际请求内容。
+如果需要通过从客户端发送不同请求来获取不同的时间格式，并且需要一个长连接，则需要进行如下改造
+*/
+func serverHandler3() {
+	service := ":1200"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleClient1(conn)
+	}
+}
+
+func handleClient1(conn net.Conn) {
+	// 设置两分钟请求超时
+	conn.SetReadDeadline(time.Now().Add(2 * time.Minute))
+	request := make([]byte, 128)
+	defer conn.Close()
+	var daytime string
+	for {
+		read_len, err := conn.Read(request)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		if read_len == 0 {
+			break
+		} else if strings.TrimSpace(string(request[:read_len])) == "timestamp" {
+			daytime = strconv.FormatInt(time.Now().Unix(), 10)
+		} else {
+			daytime = time.Now().String()
+		}
+	}
+	conn.Write([]byte(daytime))
+	request = make([]byte, 128)
+}
+
+/*
+控制TCP连接
+TCP有很多连接控制函数，常用有如下几个
+func DialTimeout(net, addr string, timeout time.Duration)(Conn, error)
+·设置建立连接的超时时间，客户端和服务端都适用，当超过设置时间时，连接自动关闭。
+func (c *TCPConn) SetReadDeadline(t time.Time) error
+func (c *TCPConn) SetWriteDealine)(t time.Time) error
+·用来设置 写入/读取 一个连接的超时时间，当超过设置时间时，连接自动关闭。
+func (c * TCPConn) SetKeepAlive(keepalive bool) os.Error
+·设置keepAlive属性，是操作系统层在tcp上没有数据和ACK的时候，会间隔性发送keepalive包，
+操作系统可以通过该包来判断一个tcp连接是否已经断开，在windows上默认2个小时没有收到数据和keepalive包的时候认为tcp连接已经断开
+更多内容可以查看net包的文档
+*/
+
+/*
+UDP Socket
+Go中处理UDPSocket和TCPSocket不同的地方就是在服务器端处理多个客户端请求数据包的方式不同，
+UDP缺少了对客户端连接请求的Accept函数。其他几乎一摸一样，只有TCP换成了UDP而已。
+几个主要函数如下所示：
+func ResolveUDPAddr(net, addr string)(*UDPAddr, os.Error)
+func DialUDP(net string, laddr, raddr *UDPAddr)(c *UDPConn, err os.Error)
+func ListenUDP(net string, laddr *UDPAddr)(c *UDPConn, err os.Error)
+func (c *UDPConn) ReadFromUDP(b []byte)(n int, addr *UDPAddr, err os.Error)
+func (c *UDPConn) WriteToUDP(b []byte, addr *UDPAddr)(n int, err os.Error)
+*/
+
+/*
+UDPSocketClient
+一个UDP的客户端代码如下所示，我们可以看到不同的就是TCP换成了UDP而已
+*/
+func UDPSocketClient() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host:port", os.Args[0])
+		os.Exit(1)
+	}
+	service := os.Args[1]
+	udpAddr, err := net.ResolveUDPAddr("udp4", service)
+	checkError(err)
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	checkError(err)
+	_, err = conn.Write([]byte("anything"))
+	checkError(err)
+	var buf [512]byte
+	n, err := conn.Read(buf[0:])
+	checkError(err)
+	fmt.Println(string(buf[0:n]))
+	os.Exit(0)
+}
+
+/*
+UDPSocketServer
+UDP服务端
+*/
+func UDPSocketServer() {
+	service := ":1200"
+	udpAddr, err := net.ResolveUDPAddr("udp4", service)
+	checkError(err)
+	conn, err := net.ListenUDP("udp", udpAddr)
+	checkError(err)
+	handleUDPClient(conn)
+}
+
+func handleUDPClient(conn *net.UDPConn) {
+	var buf [512]byte
+	_, addr, err := conn.ReadFromUDP(buf[0:])
+	if err != nil {
+		return
+	}
+	daytime := time.Now().String()
+	conn.WriteToUDP([]byte(daytime), addr)
+}
+
+/*
+通过对TCP和UDPSocket编程的描述和实现，可见Go已经完备支持了Socket编程.
+*/
