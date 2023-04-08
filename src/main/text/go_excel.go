ab0c79730195c26a67301a05f2a61d478b9659ca
Caoheng <caoheng@youlu.com>
2023-04-08 09:38:53 +0800

    feat: go处理excel

---
+++ src/main/text/go_excel.go
@@ -0,0 +1,62 @@
+package text
+
+import (
+	"fmt"
+	"github.com/xuri/excelize/v2"
+	"go_web/src/main/deploy"
+	"net/http"
+	"strings"
+)
+
+func init() {
+	deploy.Logger.Info("-- excel parse load --")
+}
+
+func ParseExcel(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		deploy.Logger.Error("不支持的请求方式")
+		fmt.Fprintln(w, "method not support")
+	}
+	r.ParseForm()
+	file, err := excelize.OpenFile(r.Form.Get("filePath"))
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		return
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			deploy.Logger.Error("文件关闭失败")
+		}
+	}()
+	list := file.GetSheetList()
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		return
+	}
+	file.RemoveRow(list[0], 1)
+	rows, err := file.Rows(list[0])
+	sql := "select * from hr_user_edu_ex where user_id in ( select user_id from mdm_prod.mdm_user where user_code in ("
+	var build strings.Builder
+	for rows.Next() {
+		row, err := rows.Columns()
+		if row[1] == "" {
+			break
+		}
+		if err != nil {
+			fmt.Println("Fatal error ", err.Error())
+			return
+		}
+		//build.WriteString(sql)
+		build.WriteString("'")
+		build.WriteString(row[0])
+		build.WriteString("'")
+		build.WriteString(",")
+	}
+	build.WriteString("))")
+	sql = sql + build.String()
+	index := strings.LastIndex(sql, ",)")
+	if index > 0 {
+		sql = strings.Replace(sql, ",)", ")", index)
+	}
+	fmt.Fprintln(w, sql)
+}
