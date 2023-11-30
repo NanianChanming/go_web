dfa6a309d2c63ee5087a9593bcde3200ca5d0c55
Caoheng <caoheng@youlu.com>
2023-11-30 16:58:19 +0800

    feat: 停止服务

--- src/main/go_gin/gin_demo.go
+++ src/main/go_gin/gin_demo.go
@@ -1,6 +1,7 @@
 package go_gin
 
 import (
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/testdata/protoexample"
@@ -8,6 +9,9 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 /*
@@ -314,4 +318,41 @@ func Reader() {
 		context.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
 	})
 	router.Run()
+}
+
+/*
+Restarted
+优雅的重启或停止
+*/
+func Restarted() {
+	router := gin.Default()
+	router.GET("/shutdown", func(ctx *gin.Context) {
+		time.Sleep(5 * time.Second)
+		ctx.String(http.StatusOK, "Welcome Gin Server")
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		// 服务连接
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// 等待中断信号以优雅地关闭服务器（设置5秒地超时时间）
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown: ", err)
+	}
+	log.Println("Server exiting")
 }
